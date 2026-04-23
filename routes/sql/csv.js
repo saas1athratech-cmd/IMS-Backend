7ce06689c509747801c3117dc28a95038a156be1
saas1athratech-cmd <saas1.athratech@gmail.com>
2026-04-23 18:17:03 +0530

    Update csv.js

--- routes/sql/csv.js
+++ routes/sql/csv.js
@@ -11,7 +11,7 @@ router.get("/inventory/export-excel",auth,exportcsv.exportInventoryExcel);
 router.get("/report/export-excel",auth,exportcsv.exportSalesExcel);
 router.get("/ladger/export-excel",auth,exportcsv.exportClientsExcel);
 router.get('/user-get/export-excel',auth,exportcsv.exportUsersExcel);
-router.get('/report-superadmin/export-excel',auth,exportcsv.downloadReport)
+router.get('/report-superadmin/export-csv',auth,exportcsv.downloadReport)
 // router.get('/ladger-client',auth,exportcsv.exportSalesExcel)
 // router.get('/')
 module.exports = router;
